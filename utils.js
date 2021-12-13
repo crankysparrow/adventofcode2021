142fdd6faba92049b69f2f5f34570807a2a0543a
Michelle <michelle.en4@gmail.com>
2021-12-12 19:39:47 -0500

    add gitignore & remove some extra stuff

--- utils.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs').promises
-
-async function inputToNums(fileName) {
-	let data = await fs.readFile(fileName, 'utf8')
-
-	return data.split('\n')
-}
-
-module.exports = { inputToNums }
